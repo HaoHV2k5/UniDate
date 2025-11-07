c99dfa208cc391a2e40c22c9ebf9189cc7258047
Nguyễn Quốc Huy <huynguyen12042005@gmail.com>
2025-11-07 17:59:56 +0700

    Date Event + Email Reminder

---
+++ BE/src/main/java/com/microsoft/hsf302_project/dto/request/RespondDateEventRequest.java
@@ -0,0 +1,8 @@
+package com.microsoft.hsf302_project.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RespondDateEventRequest {
+    private boolean accept; // true: chấp nhận, false: từ chối
+}
